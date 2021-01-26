1defe4f20623652f0b274a1c0dd8b07476a7cf9a
andreram <iramkairsingh@gmail.com>
2021-01-25 22:09:37 -0500

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+# Run the commented command manually if you haven't before
+# dotnet tool install -g Amazon.Lambda.Tools
+cd Handler/src/Handler
+dotnet build -c Release
+dotnet lambda package
+cd ../../..
+cd Frontend
+yarn build
+cd ..
+cdk deploy ProjectStack --outputs-file Frontend/build/endpoint.json
+cdk deploy FrontendStack
