e33c5e08f5e8c1c4f2483c1a791f7a3e59d295d2
eqfy <ericyan676@gmail.com>
2021-02-15 23:35:07 -0800

    Added comments about generating endpoint.json

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,4 +1,10 @@
 import data from "./endpoint.json";
+// If you are missing this file, try running deploy.sh.
+// Specifically, the following command should generate the endpoint.json file
+// ```
+// cdk deploy ProjectStack --outputs-file Frontend/src/endpoint.json
+// ```
+// see more here https://319-project.atlassian.net/wiki/spaces/PD/pages/24379393/FrontEnd+API+Gateway+Communication
 
 const config = {
     apiGateway: {
