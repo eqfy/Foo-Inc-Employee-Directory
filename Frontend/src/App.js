db45b74e8d715290405648fd15e618b8dc6141ac
andreram <49181032+andreram@users.noreply.github.com>
2021-02-16 02:17:20 -0500

    Merge frontend branches (#2)
    * barebones react app
    
    * bug fix
    
    * makes call to API endpoint now but getting null response
    
    * added body and seperated deployments
    
    * bug fix
    
    * Added the the redux, router libraries and some basic example components
    
    * delete .eslintcache
    
    * Update yarn lock
    
    * Add material-ui nav bar
    
    * Change nav bar colours to match wireframe
    
    Co-authored-by: jibril101 <mohsinak49@gmail.com>
    Co-authored-by: eric-yan02 <eric.yan02@sap.com>

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,26 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { simpleAction } from "./actions/simpleAction";
+import "./App.css";
+import { OrgChartPageContainer } from "./components/OrgChartPageContainer";
+import { ProfilePageContainer } from "./components/ProfilePageContainer";
+import { SearchPageContainer } from "./components/SearchPageContainer";
+import { AppBar, Toolbar } from '@material-ui/core';
+import styled from 'styled-components';
 
-function App() {
+function App(props) {
+  const simpleAction = (event) => {
+    props.simpleAction();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello!
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <AppBar position="static">
+            <StyledToolbar>
+              <StyledNavContainer>
+                <StyledLink to="/">Search Home</StyledLink>
+                <StyledLink to="/profile">Profile View</StyledLink>
+                <StyledLink to="/orgchart">Org Chart</StyledLink>
+              </StyledNavContainer>
+            </StyledToolbar>
+          </AppBar>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route exact path="/profile">
+              <ProfilePageContainer />
+            </Route>
+            <Route exact path="/orgchart">
+              <OrgChartPageContainer />
+            </Route>
+            <Route path="/">
+              <SearchPageContainer />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      <pre>{JSON.stringify(props)}</pre>
+      <button onClick={simpleAction}>Test redux action</button>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  ...state,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  simpleAction: () => dispatch(simpleAction()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+const StyledNavContainer = styled.div`
+  margin-left: auto;
+`;
+
+const StyledLink = styled(Link)`
+  color: black;
+  margin-left: 1rem;
+`;
+
+const StyledToolbar = styled(Toolbar)`
+  background-color: #f4f4f4;
+`;
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import configureStore from "./store";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from 'react-router-dom';
+// import { Amplify } from 'aws-amplify';
+// import config from './config';
+
+// Amplify.configure({
+//   API: {
+//     endpoints: [
+//       {
+//         name: "search",
+//         endpoint: config.apiGateway.URL,
+//         region: config.apiGateway.REGION
+//       },
+//     ]
+//   }
+// });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={configureStore()}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
