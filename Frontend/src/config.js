4574a7207d00b3b7ca6b6374ba48a090ec271996
eqfy <ericyan676@gmail.com>
2021-02-15 21:14:53 -0800

    Added aws-amplify

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,10 +1,10 @@
-import data from './endpoint.json'
+import data from "./endpoint.json";
 
 const config = {
-    apiGateway: {
-      REGION: "us-west-2",
-      URL: data.ProjectStack.ProjectapiEndpoint218845D3,
-    },
-  };
-  
-  export default config;
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: data.ProjectStack.ProjectapiEndpoint218845D3,
+  },
+};
+
+export default config;
