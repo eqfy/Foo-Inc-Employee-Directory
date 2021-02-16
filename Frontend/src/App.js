35d0bf32ff3f78fdb45a9edbb9366412098346ed
eqfy <ericyan676@gmail.com>
2021-02-15 22:54:56 -0800

    Moved redux demo to home.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import { simpleAction } from "./actions/simpleAction";
 
 import "./App.css";
 import Header from "./components/Header";
 import Routes from "./Routes";
 
 function App(props) {
-    const simpleAction = () => {
-        props.simpleAction();
-    };
-
     return (
         <div className="App">
             <Router>
                 <Header />
                 <div>{Routes()}</div>
             </Router>
-            <pre>{JSON.stringify(props)}</pre>
-            <button onClick={simpleAction}>Test redux action</button>
         </div>
     );
 }
@@ -28,8 +21,6 @@ const mapStateToProps = (state) => ({
     ...state,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-    simpleAction: () => dispatch(simpleAction()),
-});
+const mapDispatchToProps = (dispatch) => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
