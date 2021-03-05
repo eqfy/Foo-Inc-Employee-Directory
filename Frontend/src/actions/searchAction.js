4d8f5463c698efe177d0b7008502fefd7756813b
eqfy <ericyan676@gmail.com>
2021-03-05 00:35:40 -0800

    Connected experience slider to redux

--- Frontend/src/actions/searchAction.js
+++ Frontend/src/actions/searchAction.js
@@ -5,11 +5,64 @@ export const searchAction = (searchProps) => (dispatch) => {
         .then((response) => {
             console.log(response);
             dispatch({
-                type: "SEARCH_OPTION",
+                type: "ADD_WORKER",
                 payload: "employee data that satisfy filter",
             });
         })
         .catch((error) => {
             console.error("Search endpoint failed.\nErr:", error);
         });
+};
+
+export const searchByExperienceAction = (payload) => (dispatch, getState) => {
+    dispatch(setExperienceAction(payload));
+    payload = createSearchPayload(getState().appState);
+
+    console.log(
+        "Search By Experience Action dispatched.\nPayload: %o",
+        payload
+    );
+
+    searchAPI(payload)
+        .then((response) => {
+            dispatch({
+                type: "ADD_WORKER",
+                payload: response,
+            });
+        })
+        .catch((error) => {
+            console.error(
+                "Search endpoint failed (experience filter).\nErr:",
+                error
+            );
+        });
+};
+
+export const setExperienceAction = (payload) => (dispatch) => {
+    console.log(
+        "Search By Experience Action dispatched.\nPayload: %d",
+        payload
+    );
+
+    dispatch({
+        type: "SET_EXPERIENCE",
+        payload: payload,
+    });
+};
+
+const createSearchPayload = (state) => {
+    const {
+        locations,
+        titles,
+        departments,
+        companies,
+        yearsPriorExperience,
+    } = state;
+    return {
+        locations,
+        titles,
+        departments,
+        companies,
+        yearsPriorExperience,
+    };
 };
