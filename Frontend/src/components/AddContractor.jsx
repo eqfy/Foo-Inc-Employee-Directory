009171f8fdebaea7ade905dddeb73c9b22549af2
moseskirathe <moskirathe@gmail.com>
2021-03-20 11:29:06 -0700

    fix contract end date validation

--- Frontend/src/components/AddContractor.jsx
+++ Frontend/src/components/AddContractor.jsx
@@ -32,7 +32,7 @@ function AddContractor(props) {
         }
     }
 
-    const [state, setState] = React.useState({
+    const [formState, setFormState] = React.useState({
         selectedHireDate: new Date(),
         selectedTerminateDate: new Date(),
         fieldsStatusIsValid: {},
@@ -45,8 +45,8 @@ function AddContractor(props) {
         const fieldName = event.target.name;
         const fieldValue = event.target.value;
 
-        let fieldsStatusIsValid = state.fieldsStatusIsValid;
-        let errors = state.errors;
+        let fieldsStatusIsValid = formState.fieldsStatusIsValid;
+        let errors = formState.errors;
         switch(fieldName){
             case "firstName":
             case "lastName":
@@ -58,8 +58,8 @@ function AddContractor(props) {
             case "officeLocation": {
                 fieldsStatusIsValid[fieldName] = regexHelper(fieldValue, fieldName);
                 errors[fieldName] = 'Alpabetical characters only';
-                setState({
-                    ...state,
+                setFormState({
+                    ...formState,
                     fieldsStatusIsValid,
                     errors,
                 })
@@ -68,8 +68,8 @@ function AddContractor(props) {
             case "email": {
                 fieldsStatusIsValid[fieldName] = regexHelper(fieldValue, fieldName);
                 errors[fieldName] = 'Incorrect email format';
-                setState({
-                    ...state,
+                setFormState({
+                    ...formState,
                     fieldsStatusIsValid,
                     errors,
                 })
@@ -93,9 +93,12 @@ function AddContractor(props) {
             case "officeLocation":regex = '([a-zA-Z .])'; break;
             case "workPhone": 
             case "cellPhone": {} break;
-            //  case "email": regex = '^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))'; break;
+         //   case "email": regex = '^(([^<>()[.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'; break;
             default: {}
         }
+
+        if(fieldName === 'email') return new RegExp(regex).test(fieldValue);
+        
         let strLen = fieldValue.length;
         let isValid = true;
 
@@ -106,30 +109,30 @@ function AddContractor(props) {
     }
 
     const handleHireDateChange = (selectedHireDate) => {
-        setState({
-            ...state,
+        setFormState({
+            ...formState,
             selectedHireDate,
         });
       };
       
     const handleTerminateDateChange = (selectedTerminateDate) => {
-        let fieldsStatusIsValid = state.fieldsStatusIsValid;
-        let errors = state.errors;
-        fieldsStatusIsValid['terminationDate'] = !(state.selectedTerminateDate < state.selectedHireDate);
+        let fieldsStatusIsValid = formState.fieldsStatusIsValid;
+        let errors = formState.errors;
+        fieldsStatusIsValid['terminationDate'] = !(selectedTerminateDate < formState.selectedHireDate);
         errors['terminationDate'] = 'Contract end date cannot be before hire date';
-        setState({
-            ...state,
+        setFormState({
+            ...formState,
             selectedTerminateDate,
             fieldsStatusIsValid,
             errors,
         });
-};
+    };
     
     let [photoURL, setPhotoURL] = React.useState('');
-    // TODO: 4. Fix email, contract end date validation
     // TODO: 2. Connect to backend + test
     // TODO: 5. Update snackbar on success/failed, clear fields
     // TODO: 6. Cognito log in page
+    // TODO: 4. Fix email validation
     // TODO: 2. Predictive for physical location, group, company, office location
     // TODO: 7: Predictive search for supervisor
     // TODO: 8: Refractor
@@ -172,13 +175,13 @@ function uploadedPPicName(){
 }
 
 function fieldErrorHelper(fieldName){
-    return state.fieldsStatusIsValid[fieldName] === undefined? 
+    return formState.fieldsStatusIsValid[fieldName] === undefined? 
     false:
-    !state.fieldsStatusIsValid[fieldName];
+    !formState.fieldsStatusIsValid[fieldName];
 }
 
 function fieldHelperText(fieldName){
-    return !state.fieldsStatusIsValid[fieldName]? state.errors[fieldName]: '';
+    return !formState.fieldsStatusIsValid[fieldName]? formState.errors[fieldName]: '';
 }
 const handleSubmit = (event) => {
     event.preventDefault();
@@ -194,7 +197,7 @@ const handleSubmit = (event) => {
         LastName: event.target.lastName.value,
         Email: event.target.email.value,
         WorkPhone: event.target.workPhone.value,
-        WorkCell: event.target.cellPhone.value,
+        CellPhone: event.target.cellPhone.value === '+1' ? '': event.target.cellPhone.value,
         Title: event.target.title.value,
         SupervisorEmployeeNumber: "10001", // TODO: Replace with  actual supervisor employee number once we have predictive search
         HireDate: event.target.hireDate.value,
@@ -274,7 +277,6 @@ const handleSubmit = (event) => {
                     name="cellPhone" 
                     variant="outlined" 
                     size="small" 
-                    required
                     />
                 </Grid>
                 <input
@@ -328,7 +330,7 @@ const handleSubmit = (event) => {
                             format="MM/dd/yyyy"
                             margin="normal"
                             label="Hire Date"
-                            value={state.selectedHireDate}
+                            value={formState.selectedHireDate}
                             onChange={handleHireDateChange}
                             KeyboardButtonProps={{
                                 'aria-label': 'change date',
@@ -348,7 +350,7 @@ const handleSubmit = (event) => {
                             margin="normal"
                             id="date-picker-inline"
                             label="Contract End Date"
-                            value={state.selectedTerminateDate}
+                            value={formState.selectedTerminateDate}
                             onChange={handleTerminateDateChange}
                             KeyboardButtonProps={{
                                 'aria-label': 'change date',
