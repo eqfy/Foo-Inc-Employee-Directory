4efae32e178cb3f204a5a7dba4203d7c80612a4f
eqfy <ericyan676@gmail.com>
2021-03-22 00:31:38 -0700

    Fixed update notes

--- Frontend/src/components/UpdatePageContainer.jsx
+++ Frontend/src/components/UpdatePageContainer.jsx
@@ -5,33 +5,51 @@ export default function UpdatePageContainer(props) {
     return (
         <PageContainer>
             <div>
-                <h1>Mar 14th</h1>
+                <h1>Mar 21</h1>
                 <h2>Below are some visible changes since last deployment:</h2>
                 <h3>
                     Search Page:
                     <ul>
                         <li>
-                            filter area is now responsive to filter changes in
-                            apply filter area and vice versa
-                        </li>
-                        <li>
-                            typing in a filter/category in the apply filter area
+                            Typing in a filter/category in the apply filter area
                             will return a list of potential filters to apply. If
                             there is only one filter, pressing "Enter" selects
-                            the filter
+                            the filter.
                         </li>
                         <li>
-                            all filters changes now toggle a request to the
+                            All filters changes now toggle a request to the
                             backend which will update the result area with
-                            workers satifying the filters
+                            workers satifying the filters.
+                        </li>
+                        <li>
+                            Predictive search by name is the final API
+                            connection that remains to be implemented.
+                        </li>
+                    </ul>
+                    Profile View:
+                    <ul>
+                        <li></li>
+                    </ul>
+                    Org Chart:
+                    <ul>
+                        <li></li>
+                    </ul>
+                </h3>
+            </div>
+            <div>
+                <h1>Mar 14</h1>
+                <h2>Below are some visible changes since last deployment:</h2>
+                <h3>
+                    Search Page:
+                    <ul>
+                        <li>
+                            filter area is now responsive to filter changes,
+                            though not yet connected to backend search endpoint
                         </li>
                         <li>
                             result area is connected to redux store, and s3
                             buckets has been set up for images (though not all
                             mock data have the url)
-                        </li>
-                        <li>
-                            predictive search by name is yet to be implemented
                         </li>
                     </ul>
                     Profile View:
