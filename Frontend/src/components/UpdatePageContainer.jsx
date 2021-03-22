f533c092ba7c801ee57d82f928a1b1ce5e164bee
Maxwell-Yang-2001 <migacanada@gmail.com>
2021-03-22 01:35:10 -0700

    Update update tab

--- Frontend/src/components/UpdatePageContainer.jsx
+++ Frontend/src/components/UpdatePageContainer.jsx
@@ -6,6 +6,7 @@ export default function UpdatePageContainer(props) {
         <PageContainer>
             <div>
                 <h1>Mar 21</h1>
+                <h3>Note that some of the loading takes around 15 seconds. This is due to the cold start for each API endpoints if there is no action in 5 minutes. This would be improved later.</h3>
                 <h2>Below are some visible changes since last deployment:</h2>
                 <h3>
                     Search Page:
@@ -25,14 +26,28 @@ export default function UpdatePageContainer(props) {
                             Predictive search by name is the final API
                             connection that remains to be implemented.
                         </li>
+                        <li>
+                            Employee card has a better UI now. It would have 1 line for both name and title, and hides long text unless hover.
+                        </li>
                     </ul>
                     Profile View:
                     <ul>
-                        <li></li>
+                        <li>
+                            Profile view is now connected to the backend.
+                        </li>
                     </ul>
                     Org Chart:
                     <ul>
-                        <li></li>
+                        <li>
+                            Org chart search by name bar is connected to the backend. Entering at least 2 characters will fetch at most 20 entries with matching names in the dropdown. 
+                        </li>
+                        <li>
+                            Org chart has an updated UI. It would display long text in the same place as the short one when hover, if a string is too long.
+                        </li>
+                    </ul>
+                    Add Contractor:
+                    <ul>
+                        <li>Has basic form structure, currently hooking to backend and redux store</li>
                     </ul>
                 </h3>
             </div>
@@ -44,18 +59,18 @@ export default function UpdatePageContainer(props) {
                     <ul>
                         <li>
                             filter area is now responsive to filter changes,
-                            though not yet connected to backend search endpoint
+                            though not yet connected to backend search endpoint.
                         </li>
                         <li>
                             result area is connected to redux store, and s3
                             buckets has been set up for images (though not all
-                            mock data have the url)
+                            mock data have the url).
                         </li>
                     </ul>
                     Profile View:
                     <ul>
                         <li>
-                            Previous / Next button is connected to redux store
+                            Previous / Next button is connected to redux store.
                         </li>
                     </ul>
                     Org Chart:
@@ -63,7 +78,7 @@ export default function UpdatePageContainer(props) {
                         <li>
                             Org chart has a better UI now. It is connected to
                             backend, and user can rerender by clicking nodes.
-                            Grab and zoom are supported
+                            Grab and zoom are supported.
                         </li>
                     </ul>
                 </h3>
