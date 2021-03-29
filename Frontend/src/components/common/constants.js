b86d75103e49d7ef982c2cd220a3c6c18fccd22d
eqfy <ericyan676@gmail.com>
2021-03-29 09:31:38 -0700

    Some update to constants file

--- Frontend/src/components/common/constants.js
+++ Frontend/src/components/common/constants.js
@@ -1,13 +1,10 @@
-// Search Page
-export const SEARCH_AREA_WIDTH = 270;
-
 export const DEFAULT_SEARCH_TIMEOUT = 1000;
-export const MAX_WORK_EXPERIENCE = 30;
+export const MAX_WORK_EXPERIENCE = 20;
 export const PagePathEnum = {
-  SEARCH: "/search",
-  PROFILE: "/profile",
-  ORGCHART: "/orgchart",
-  NEWCONTRACTOR: "/addContractor",
+    SEARCH: "/search",
+    PROFILE: "/profile",
+    ORGCHART: "/orgchart",
+    NEWCONTRACTOR: "/addContractor",
     // TODO: remove after update
     UPDATE: "/update",
-}
+};
