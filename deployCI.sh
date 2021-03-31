7d4da2a37dccbf44f968d1d9cc07b1a0ac2bad98
andreram <iramkairsingh@gmail.com>
2021-03-31 19:48:06 -0400

    Change CI build order

--- deployCI.sh
+++ deployCI.sh
@@ -3,14 +3,13 @@ cd Handler/src/Handler
 echo "***Building backend***"
 dotnet build -c Release
 dotnet lambda package
-cd ../../..
-echo "***Deploying backend***"
-cdk deploy ProjectStack --outputs-file Frontend/src/endpoint.json --require-approval never
-cd Frontend
+cd ../../../Frontend
 echo "***Building frontend***"
 yarn install
 CI='' yarn build
 cd ..
+echo "***Deploying backend***"
+cdk deploy ProjectStack --outputs-file Frontend/src/endpoint.json --require-approval never
 echo "***Deploying frontend***"
 cdk deploy FrontendStack --require-approval never
 echo "Done"
