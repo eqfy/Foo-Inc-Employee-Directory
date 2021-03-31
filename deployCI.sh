40b4e742223a5ecf8c2dab23e136921f44c2f770
andreram <iramkairsingh@gmail.com>
2021-03-31 15:31:27 -0400

    CI test 6

---
+++ deployCI.sh
@@ -0,0 +1,16 @@
+dotnet tool install -g Amazon.Lambda.Tools
+cd Handler/src/Handler
+echo "***Building backend***"
+dotnet build -c Release
+dotnet lambda package
+cd ../../..
+echo "***Deploying backend***"
+cdk deploy ProjectStack --outputs-file Frontend/src/endpoint.json --require-approval never
+cd Frontend
+echo "***Building frontend***"
+yarn install
+yarn build
+cd ..
+echo "***Deploying frontend***"
+cdk deploy FrontendStack --require-approval never
+echo "Done"
