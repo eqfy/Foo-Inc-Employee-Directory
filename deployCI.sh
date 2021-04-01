42a5d00abb75c093a8d95977f46762ee53ae7062
andreram <iramkairsingh@gmail.com>
2021-03-31 21:56:39 -0400

    Revert "Remove hardcoded endpoint.json file"
    This reverts commit 9fa3a5945e302d34b59e67f55ff2f53962094348.

--- deployCI.sh
+++ deployCI.sh
@@ -3,14 +3,13 @@ cd Handler/src/Handler
 echo "***Building backend***"
 dotnet build -c Release
 dotnet lambda package
-cd ../../..
-echo "***Deploying backend***"
-cdk deploy ProjectStack --outputs-file Frontend/src/endpoint.json --require-approval never || exit 1
-cd Frontend
+cd ../../../Frontend
 echo "***Building frontend***"
 yarn install
-yarn build
+CI='' yarn build
 cd ..
+echo "***Deploying backend***"
+cdk deploy ProjectStack --outputs-file Frontend/src/endpoint.json --require-approval never || exit 1
 echo "***Deploying frontend***"
 cdk deploy FrontendStack --require-approval never || exit 1
 echo "Done"
