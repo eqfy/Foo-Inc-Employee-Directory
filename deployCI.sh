1c08434b6e083ce8c0d1a4d0fa9e8002c31ab928
andreram <iramkairsingh@gmail.com>
2021-03-31 20:09:48 -0400

    Changed CI account build permissions

--- deployCI.sh
+++ deployCI.sh
@@ -9,7 +9,7 @@ yarn install
 CI='' yarn build
 cd ..
 echo "***Deploying backend***"
-cdk deploy ProjectStack --outputs-file Frontend/src/endpoint.json --require-approval never
+cdk deploy ProjectStack --outputs-file Frontend/src/endpoint.json --require-approval never || exit 1
 echo "***Deploying frontend***"
-cdk deploy FrontendStack --require-approval never
+cdk deploy FrontendStack --require-approval never || exit 1
 echo "Done"
