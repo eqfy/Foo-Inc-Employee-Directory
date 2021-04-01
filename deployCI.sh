9fa3a5945e302d34b59e67f55ff2f53962094348
andreram <iramkairsingh@gmail.com>
2021-03-31 21:52:55 -0400

    Remove hardcoded endpoint.json file

--- deployCI.sh
+++ deployCI.sh
@@ -3,13 +3,14 @@ cd Handler/src/Handler
 echo "***Building backend***"
 dotnet build -c Release
 dotnet lambda package
-cd ../../../Frontend
-echo "***Building frontend***"
-yarn install
-CI='' yarn build
-cd ..
+cd ../../..
 echo "***Deploying backend***"
 cdk deploy ProjectStack --outputs-file Frontend/src/endpoint.json --require-approval never || exit 1
+cd Frontend
+echo "***Building frontend***"
+yarn install
+yarn build
+cd ..
 echo "***Deploying frontend***"
 cdk deploy FrontendStack --require-approval never || exit 1
 echo "Done"
