73ae2e3267d3f60284965deea50660acb66626b3
jibril101 <mohsinak49@gmail.com>
2021-04-07 17:26:44 -0700

    exit after fail

--- createAdmin.sh
+++ createAdmin.sh
@@ -3,10 +3,12 @@
 if [ -z "$1" ]
     then
         echo "Please Enter A Username"
+        exit 1
 fi
 if [ -z "$2" ]
     then 
         echo "Please Enter a Password"
+        exit 1
 fi
 POOL=$(aws cognito-idp list-user-pools --max-results 1 | awk '/Id/ {print $2}' | tr -d , | sed -e 's/^"//' -e 's/"$//')
 aws cognito-idp admin-create-user --user-pool-id ${POOL} --username $1
