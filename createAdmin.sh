1bf5e8e23d98028897a8716dfc6be695da189e92
jibril101 <mohsinak49@gmail.com>
2021-04-07 17:24:43 -0700

    check for user and pass arg

--- createAdmin.sh
+++ createAdmin.sh
@@ -1,5 +1,13 @@
 
 #!/bin/bash
+if [ -z "$1" ]
+    then
+        echo "Please Enter A Username"
+fi
+if [ -z "$2" ]
+    then 
+        echo "Please Enter a Password"
+fi
 POOL=$(aws cognito-idp list-user-pools --max-results 1 | awk '/Id/ {print $2}' | tr -d , | sed -e 's/^"//' -e 's/"$//')
 aws cognito-idp admin-create-user --user-pool-id ${POOL} --username $1
 aws cognito-idp admin-set-user-password --user-pool-id ${POOL} --username $1 --password $2 --permanent
