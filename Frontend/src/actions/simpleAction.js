2924e893e6721a45ac2509d67e5c188f826f4367
eqfy <ericyan676@gmail.com>
2021-04-12 20:03:03 -0700

    Deleted unused files

--- Frontend/src/actions/simpleAction.js
+++
@@ -1,7 +0,0 @@
-export const simpleAction = (context) => (dispatch) => {
-    console.log(context);
-    dispatch({
-        type: "SIMPLE_ACTION",
-        payload: "result_of_simple_action",
-    });
-};
