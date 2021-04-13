6d053b434963c9e7e813cccb2abbdf5edf9bb98a
andreram <iramkairsingh@gmail.com>
2021-04-13 16:14:07 -0400

    remove old script

--- deployBackend.sh
+++ deployBackend.sh
@@ -1,3 +1,31 @@
+while getopts "p:e:c" opt; do
+  case $opt in
+    p)
+        userProfile=$OPTARG
+        echo "User profile set to: $userProfile"
+      ;;
+    e)
+      export DEPLOY_ENVIRONMENT=$OPTARG
+      echo "Deploy environment: $DEPLOY_ENVIRONMENT" 
+      copy /b src/Project/ProjectStack.cs +,,
+      ;;
+    c)
+      clean=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+
+
+
 # Run the commented command manually if you haven't before
 # dotnet tool install -g Amazon.Lambda.Tools
 cd Handler/src/Handler
@@ -6,5 +34,23 @@ dotnet build -c Release
 dotnet lambda package
 cd ../../..
 echo "***Deploying backend***"
-cdk deploy ProjectStack --outputs-file Frontend/src/endpoint.json --require-approval never
-echo "Done"
+if [ $userProfile ]
+then
+    cdk deploy ProjectStack --outputs-file Frontend/src/endpoint.json --require-approval never --profile $userProfile
+    if [ $clean ]
+    then
+        dbinitName=`aws lambda list-functions --profile $userProfile | awk '/ProjectStack-databaseInit/ {print $2}' | grep -v arn | tr -d , | sed -e 's/^"//' -e 's/"$//'`
+        dropTablesName=`aws lambda list-functions --profile $userProfile | awk '/ProjectStack-databaseDropAll/ {print $2}' | grep -v arn | tr -d , | sed -e 's/^"//' -e 's/"$//'`
+        aws lambda invoke --function-name $dropTablesName --profile $userProfile response.json
+        aws lambda invoke --function-name $dbinitName --profile $userProfile response.json
+    fi
+else
+    cdk deploy ProjectStack --outputs-file Frontend/src/endpoint.json --require-approval never
+    if [ $clean ]
+    then
+        dbinitName=`aws lambda list-functions | awk '/ProjectStack-databaseInit/ {print $2}' | grep -v arn | tr -d , | sed -e 's/^"//' -e 's/"$//'`
+        dropTablesName=`aws lambda list-functions | awk '/ProjectStack-databaseDropAll/ {print $2}' | grep -v arn | tr -d , | sed -e 's/^"//' -e 's/"$//'`
+        aws lambda invoke --function-name $dropTablesName sresponse.json
+        aws lambda invoke --function-name $dbinitName response.json
+    fi
+fi
